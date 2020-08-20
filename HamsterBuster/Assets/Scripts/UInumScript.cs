74db22ec9d550548e1145779d727abaa169a60de
MikumiNishi <c20020@std.it-college.ac.jp>
2020-08-20 23:33:57 +0900

    残りの高さと現在の敵（怒ったハムスター）の数を表示するUIを作成しました。雑ですまん

---
+++ HamsterBuster/Assets/Scripts/UInumScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//書いた人: みくみ なんかよくわかんないけどテストシーンでは動いた
+
+public class UInumScript : MonoBehaviour
+{
+    GameObject[] humster;
+
+    public Text humsterNum;
+    public Text CurrentHeight;
+    public int CurrentHeight_num = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Check();
+    }
+
+    void Check()
+    {
+        int counthum = GameObject.FindGameObjectsWithTag("humster").Length;
+        humsterNum.text = (counthum.ToString() + "匹");
+
+        int countheight = (GameObject.FindGameObjectsWithTag("building").Length + 2);  //1段の高さを3mにしたいので+2してみた
+        CurrentHeight.text = (countheight.ToString() + "m");
+
+    }
+}
