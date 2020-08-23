a035c9e4e56822f19d0def594ea607bc4c8a5b39
MikumiNishi <c20020@std.it-college.ac.jp>
2020-08-23 22:14:29 +0900

    BGMというシーンでBGMコントローラーと飼育員が投げるときにスペースを押すとSEがなるやつだけ追加しました。

---
+++ HamsterBuster/Assets/Scripts/SEshiikuin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEshiikuin : MonoBehaviour
+{
+    public AudioClip shootsound;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getConponent
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Spacekey");
+            audioSource.PlayOneShot(shootsound);
+        }
+    }
+}
