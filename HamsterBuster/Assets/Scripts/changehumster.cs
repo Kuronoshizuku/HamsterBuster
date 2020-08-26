857d9897e10a7bf01b18f6081491944da5082138
ein0920 <c20026@std.it-college.ac.jp>
2020-08-26 14:40:01 +0900

    ビルのスクリプトの追加

---
+++ HamsterBuster/Assets/Scripts/changehumster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changehumster : MonoBehaviour
+{
+    Material[] Prefabs;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Resourcesフォルダからマテリアルを読み込む
+        Prefabs = Resources.LoadAll<Material>("Prefabs/");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //"humster"タグが付いているオブジェクトのマテリアルを変更する
+        if (collision.gameObject.tag == "humster")
+        {
+            //オブジェクトのマテリアルを変更する
+            gameObject.GetComponent<Renderer>().material = Prefabs[0];
+            collision.gameObject.GetComponent<Renderer>().material = Prefabs[1];
+            collision.gameObject.GetComponent<Renderer>().material = Prefabs[2];
+        }
+
+    }
+
+}
