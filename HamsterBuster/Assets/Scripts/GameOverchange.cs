f368699ea181b8231353e1406aa8b403863d0f08
MikumiNishi <c20020@std.it-college.ac.jp>
2020-08-30 00:03:56 +0900

    ボタンが反応しないのでゲームオーバーから12秒後に強制的にタイトルにとばすスクリプトはりつけた

---
+++ HamsterBuster/Assets/Scripts/GameOverchange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//書いた人 みくみ
+//なぜかボタンが反応しないので12秒立つと強制的にタイトルに飛ばすようにしました。
+
+public class GameOverchange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(12);
+
+        SceneManager.LoadScene("TitleScene");
+
+    }
+}
